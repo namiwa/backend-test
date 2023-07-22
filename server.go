0712717278658bb66dd690cdf453367f5a418d08
namiwa <khairul.nasri895@gmail.com>
2023-07-22 14:31:52 +0800

    feat: add recover middleware

--- server.go
+++ server.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
 
 func main() {
 	app := fiber.New()
 
+	app.Use(recover.New())
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Backend is very healthy!")
 	})
 
-	app.Listen(":3000")
+	log.Fatal(app.Listen(":3000"))
 }
